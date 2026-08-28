57762720c0885a88caefc725081c4c500e4347cc
Alice Driscoll <alicedcodes@gmail.com>
2026-08-28 13:49:04 +1000

    feat: update about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -35,9 +35,14 @@ export const AboutPage = () => {
 
       <p>
         I'm an amateur web developer from <b>Brisbane, Australia</b>, where it is{" "}
-        <b>{formattedTime()}</b> on a <b>{formattedDate()}</b>. I use tools like <b>SolidJS</b> and{" "}
-        <b>TailwindCSS</b> to create websites like this one. You can check out my code at my{" "}
-        <a href="https://github.com/alicedcodes">GitHub profile</a>.
+        <b>{formattedTime()}</b> on a <b>{formattedDate()}</b>. I'm <b>genderfluid</b>, though I
+        tend to lean towards <b>she/her</b> pronouns.
+      </p>
+
+      <p>
+        I use tools like <b>SolidJS</b> and <b>TailwindCSS</b> to create websites like this one. You
+        can check out my code at my <a href="https://github.com/alicedcodes">GitHub profile</a>. My
+        editor of choice is <b>Zed</b>, and my preferred OS is <b>Fedora Linux</b>.
       </p>
     </main>
   );
