7653cd561513927096324b00cb18fcf96ed2a9f8
Alice Driscoll <alicedcodes@gmail.com>
2026-09-04 21:53:54 +1000

    feat: add app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return <p class="py-20 text-center text-4xl text-green-700">Hello tailwind!</p>;
+  return (
+    <div class="flex min-h-dvh flex-col bg-app text-text">
+      <nav class="z-10 h-12 bg-header shadow"></nav>
+      <div class="flex-auto p-8">
+        <main class="mx-auto max-w-4xl"></main>
+      </div>
+    </div>
+  );
 };
 
 export default App;
