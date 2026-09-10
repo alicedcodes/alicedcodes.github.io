5b4da7fdd730f3df1b47f5b17df7ca2fd8a314a5
Alice Driscoll <alicedcodes@gmail.com>
2026-09-10 15:16:17 +1000

    feat: update projects section, add infinite minesweeper to projects section

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,4 +1,21 @@
-import type { Component } from "solid-js";
+import { For, type Component } from "solid-js";
+
+interface Project {
+  id: string;
+  name: string;
+  description: string;
+  link: string;
+}
+
+const PROJECTS: Project[] = [
+  {
+    id: "infinite-minesweeper",
+    name: "Infinite Minesweeper",
+    description:
+      "An infinitely playable game of Minesweeper with features that provider a smoother experience. Playable on the web, it is completely free without sacrificing quality.",
+    link: "https://alicedcodes.github.io/infinite-minesweeper",
+  },
+];
 
 export const ProjectsSection: Component = () => {
   return (
@@ -9,6 +26,28 @@ export const ProjectsSection: Component = () => {
         </a>
         Projects
       </h2>
+
+      <For each={PROJECTS}>
+        {(project) => (
+          <>
+            <h3 id={project.id}>
+              <a href={`#${project.id}`} class="pr-2">
+                #
+              </a>
+              {project.name}
+            </h3>
+
+            <a
+              href={project.link}
+              class="rounded-full bg-button px-4 py-1 text-xl font-bold text-text no-underline shadow"
+            >
+              GO!
+            </a>
+
+            <p>{project.description}</p>
+          </>
+        )}
+      </For>
     </>
   );
 };
