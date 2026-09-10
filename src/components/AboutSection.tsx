94d55abe30432477f5c808b4c0ebc43d6c2c9008
Alice Driscoll <alicedcodes@gmail.com>
2026-09-10 15:15:52 +1000

    feat: add timezone info to about section, reword some setences

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,6 +1,34 @@
-import type { Component } from "solid-js";
+import { createMemo, createSignal, onCleanup, onMount, type Component } from "solid-js";
 
 export const AboutSection: Component = () => {
+  const [now, setNow] = createSignal(Date.now());
+
+  onMount(() => {
+    const timer = setInterval(() => setNow(Date.now()), 15000);
+    onCleanup(() => clearInterval(timer));
+  });
+
+  const formattedNow = createMemo(() => {
+    const d = new Date(now());
+    return {
+      time: d.toLocaleString(navigator.language, {
+        timeZone: "Australia/Brisbane",
+        hour: "numeric",
+        minute: "2-digit",
+      }),
+      weekday: d.toLocaleString(navigator.language, {
+        timeZone: "Australia/Brisbane",
+        weekday: "long",
+      }),
+      date: d.toLocaleString(navigator.language, {
+        timeZone: "Australia/Brisbane",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
   return (
     <>
       <h2 id="about">
@@ -11,13 +39,14 @@ export const AboutSection: Component = () => {
       </h2>
 
       <p>
-        Hey, I'm <b>Alice</b>, an amateur <b>web developer</b> from <b>Brisbane, Australia</b>. I'm
-        genderfluid and use she/he/they/it pronouns.
+        Hey, I'm <b>Alice</b>, an amateur <b>web developer</b> from <b>Brisbane, Australia</b>,
+        where it is <b>{formattedNow().time}</b> on <b>{formattedNow().date}</b>. I'm genderfluid
+        and use she/he/they/it pronouns.
       </p>
 
       <p>
-        I write websites in TypeScript using Solid JS and Tailwind CSS. My preferred editor is Zed
-        and my current OS is Fedora Linux.
+        I write websites like this one in TypeScript using Solid JS and Tailwind CSS. I use the Zed
+        editor with Fedora Linux as my OS.
       </p>
     </>
   );
